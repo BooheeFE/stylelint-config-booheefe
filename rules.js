4c8e03677df3125e40841cfa2a20550b8aec6879
simbawu <code@simbawu.com>
2018-11-08 15:12:41 +0800

    feat: change unknown selector

--- rules.js
+++ rules.js
@@ -115,7 +115,9 @@ module.exports = {
   "selector-list-comma-space-before": "never",
   "selector-max-empty-lines": 0,
   "selector-pseudo-class-case": "lower",
-  "selector-pseudo-class-no-unknown": true,
+  "selector-pseudo-class-no-unknown": [true: {
+    "ignorePseudoClasses": ["global"]
+  }],
   "selector-pseudo-class-parentheses-space-inside": "never",
   "selector-pseudo-element-case": "lower",
   "selector-pseudo-element-colon-notation": "double",
