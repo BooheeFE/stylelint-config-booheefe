dcd1ba04314734f51ea08b1de700322db595921a
simbawu <code@simbawu.com>
2018-11-22 14:18:18 +0800

    feat: change no-descending-specificity rule

--- rules.js
+++ rules.js
@@ -89,7 +89,7 @@ module.exports = {
   "media-query-list-comma-newline-after": "always-multi-line",
   "media-query-list-comma-space-after": "always-single-line",
   "media-query-list-comma-space-before": "never",
-  "no-descending-specificity": true,
+  "no-descending-specificity": null,
   "no-duplicate-at-import-rules": true,
   "no-duplicate-selectors": true,
   "no-empty-source": null,
